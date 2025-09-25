4b9cd5e1d4818ccb74807220a620d85beeb11695
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-09-25 14:16:04 +0200

    Android: Fix preserve ViewModel across configuration changes

--- AndroidClient/OSMediaMote/app/src/main/java/com/xdmpx/osmediamote/MainActivity.kt
+++ AndroidClient/OSMediaMote/app/src/main/java/com/xdmpx/osmediamote/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -30,10 +31,8 @@ import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -69,7 +68,7 @@ val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "os
 class MainActivity : ComponentActivity() {
     private lateinit var volleyQueue: RequestQueue
     private var updateTimer: Timer? = null
-    private val osMediaMoteViewModel = OSMediaMote()
+    private val osMediaMoteViewModel by viewModels<OSMediaMote>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
