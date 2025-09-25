67dd76c3b48c0eb2aa0b86f9c90a947cc28a0c31
xDMPx <57721731+xDMPx@users.noreply.github.com>
2025-09-25 17:02:21 +0200

    Add basic CLI client

---
+++ osmediamote-cli/src/lib.rs
@@ -0,0 +1,75 @@
+#[inline(always)]
+pub fn get_reqwest_client() -> reqwest::Result<reqwest::blocking::Client> {
+    let user_agent: String = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+
+    let reqwest_client = reqwest::blocking::Client::builder()
+        .timeout(std::time::Duration::from_secs(1))
+        .user_agent(user_agent)
+        .build()?;
+
+    Ok(reqwest_client)
+}
+
+#[inline(always)]
+pub fn reqwest_get(url: &str) -> reqwest::Result<reqwest::blocking::Response> {
+    let reqwest_client = get_reqwest_client()?;
+
+    let request = reqwest_client.get(url).build()?;
+    let response = reqwest_client.execute(request)?;
+
+    Ok(response)
+}
+
+#[derive(Debug)]
+pub enum OSMediaMoteError {
+    InvalidOption(String),
+    InvalidOptionsStructure,
+    ReqwestError(reqwest::Error),
+}
+
+#[derive(PartialEq)]
+pub enum ProgramOption {
+    IP(String),
+    Play,
+    Pause,
+    PlayPause,
+    Next,
+    Previous,
+    Status,
+    Position,
+}
+
+pub fn process_args() -> Result<Vec<ProgramOption>, OSMediaMoteError> {
+    let mut options = vec![];
+    let mut args: Vec<String> = std::env::args().skip(1).collect();
+    args.reverse();
+
+    let last_arg = args
+        .pop()
+        .ok_or(OSMediaMoteError::InvalidOptionsStructure)?;
+    let ip = last_arg;
+    if ip.chars().filter(|&c| c == '.').count() != 3 {
+        return Err(OSMediaMoteError::InvalidOptionsStructure);
+    }
+    options.push(ProgramOption::IP(ip));
+
+    for arg in args {
+        let arg = match arg.as_str() {
+            "play" => Ok(ProgramOption::Play),
+            "pause" => Ok(ProgramOption::Pause),
+            "play-pause" => Ok(ProgramOption::PlayPause),
+            "next" => Ok(ProgramOption::Next),
+            "previous" => Ok(ProgramOption::Previous),
+            "status" => Ok(ProgramOption::Status),
+            "position" => Ok(ProgramOption::Position),
+            _ => Err(OSMediaMoteError::InvalidOption(arg)),
+        };
+        options.push(arg?);
+    }
+
+    if options.len() != 2 {
+        return Err(OSMediaMoteError::InvalidOptionsStructure);
+    }
+
+    Ok(options)
+}
---
+++ osmediamote-cli/src/main.rs
@@ -0,0 +1,55 @@
+use osmediamote_cli::{ProgramOption, process_args, reqwest_get};
+
+fn main() {
+    let options = process_args()
+        .map_err(|err| {
+            match err {
+                osmediamote_cli::OSMediaMoteError::InvalidOption(option) => {
+                    eprintln!("Provided option {option} is invalid")
+                }
+                osmediamote_cli::OSMediaMoteError::InvalidOptionsStructure => {
+                    eprintln!("Invalid input")
+                }
+                _ => panic!("{:?}", err),
+            }
+            std::process::exit(-1);
+        })
+        .unwrap();
+
+    let ip = options
+        .iter()
+        .find_map(|o| match o {
+            ProgramOption::IP(ip) => Some(ip.to_owned()),
+            _ => None,
+        })
+        .unwrap();
+
+    for option in options {
+        match option {
+            ProgramOption::IP(_) => (),
+            ProgramOption::Play => {
+                let _ = reqwest_get(&format!("http://{ip}:65420/play")).unwrap();
+            }
+            ProgramOption::Pause => {
+                let _ = reqwest_get(&format!("http://{ip}:65420/pause")).unwrap();
+            }
+            ProgramOption::PlayPause => {
+                let _ = reqwest_get(&format!("http://{ip}:65420/play_pause")).unwrap();
+            }
+            ProgramOption::Next => {
+                let _ = reqwest_get(&format!("http://{ip}:65420/play_next")).unwrap();
+            }
+            ProgramOption::Previous => {
+                let _ = reqwest_get(&format!("http://{ip}:65420/play_prev")).unwrap();
+            }
+            ProgramOption::Status => {
+                let res = reqwest_get(&format!("http://{ip}:65420/is_playing")).unwrap();
+                println!("Playing: {}", res.text().unwrap());
+            }
+            ProgramOption::Position => {
+                let res = reqwest_get(&format!("http://{ip}:65420/position")).unwrap();
+                println!("{}", res.text().unwrap());
+            }
+        }
+    }
+}
