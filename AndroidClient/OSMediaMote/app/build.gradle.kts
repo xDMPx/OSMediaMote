5b344234984ca2823a593e9f919a6de5b4c73c55
xDMPx <57721731+xDMPx@users.noreply.github.com>
2026-02-02 15:38:17 +0100

    Android: Fully migrate to AGP 9.0.0 and new DSL. Remove gradle migration properties. Bump dependencies.

--- AndroidClient/OSMediaMote/app/build.gradle.kts
+++ AndroidClient/OSMediaMote/app/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.xdmpx.osmediamote"
-    compileSdk = 36
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         applicationId = "com.xdmpx.osmediamote"
@@ -31,9 +32,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -51,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.compose.material.icons)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
